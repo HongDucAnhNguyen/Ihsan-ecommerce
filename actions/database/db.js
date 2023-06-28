8138d5debda66cee9c87d1a44b33a18679505f34
Jay Nguyen <bobguy0606@gmail.com>
2023-06-28 01:41:48 -0700

    weird production url is undefined but somehow still works

--- actions/database/db.js
+++ actions/database/db.js
@@ -1,6 +1,7 @@
 const connectMongo = async () => {
   try {
-    await fetch("/api/connectDB");
+    await fetch(`${process.env.BASE_APP_URL}/api/connectDB`);
+    return;
   } catch (error) {
     console.log(error);
   }

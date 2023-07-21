7ed19b815618ea2a925b173770aeec6b52c1cfb4
Jay Nguyen <bobguy0606@gmail.com>
2023-07-21 09:54:37 -0700

    new layout done

--- pageSections/components/Products.js
+++ pageSections/components/Products.js
@@ -30,29 +30,13 @@ const Products = ({ products }) => {
     <motion.div variants={fadeInVariants} initial="initial" animate="animate">
       <Box
         className={productStyles.productsLayoutContainer}
-        // spacing={10}
-        // columns={{ xs: 1, sm: 2, md: 3 }}
-        // sx={{
-        //   "&::-webkit-scrollbar": {
-        //     height: "9px",
-        //     borderRadius: "8px",
-        //   },
-        //   "&::-webkit-scrollbar-track": {
-        //     background: "gray.100",
-        //   },
-        //   "&::-webkit-scrollbar-thumb": {
-        //     backgroundColor: "gray.600",
-        //     borderRadius: "8px",
-        //   },
-        // }}
+       
       >
         {products.length > 0 &&
           products.map((product) => (
             <Product key={product._id} product={product}></Product>
           ))}
-        {/* {!isLoading && products.length === 0 && (
-          <Text fontSize="2xl">No Products availabled</Text>
-        )} */}
+        
       </Box>
     </motion.div>
   );
